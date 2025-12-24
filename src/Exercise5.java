2460c373d14b629d9b058adecd2848dc71f6b83a
Nodirbek <n.nurkulov@insuranceon.uz>
2025-12-24 18:00:09 +0500

    Exercise7 started

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -11,42 +11,35 @@ import java.util.Scanner;
 public class Exercise5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the number m: ");
+        System.out.print("m (rows): ");
         int m = input.nextInt();
 
-        System.out.print("Enter the number n: ");
+        System.out.print("n (columns): ");
         int n = input.nextInt();
 
-        System.out.print("Enter the number d: ");
+        System.out.print("d (difference): ");
         int d = input.nextInt();
 
-        int[] numsM = new int[m];
-        System.out.println("Enter " + m + " element(s).");
+        int[] firstCol = new int[m];
+        System.out.print("Enter " + m + " element(s): ");
         for (int i = 0; i < m; i++) {
-            System.out.print("Enter " + i + " - element: ");
-            numsM[i] = input.nextInt();
-        }
-
-        System.out.println("Elements that you entered are: ");
-        for (int i = 0; i < m; i++) {
-            System.out.print(numsM[i] + " ");
+            firstCol[i] = input.nextInt();
         }
 
         int[][] matrix = new int[m][n];
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (j == 0) {
-                    matrix[i][j] = numsM[i-1];
-                } else {
-                    matrix[i][j] = matrix[i][j] + d;
-                }
+
+        for (int i = 0; i < m; i++) {
+            matrix[i][0] = firstCol[i];
+
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i][j - 1] + d;
             }
         }
 
         System.out.println();
         System.out.println("The matrix after changing: ");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j] + "\t");
             }
             System.out.println();
--- src/Exercise7.java
+++ src/Exercise7.java
@@ -1,8 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
- * 7. m x n o'lchamli matritsa va k soni berilgan (0 <= k < n ). Matritsaning k- ustuni elementlarini chiqaruvchi programma tuzilsin.
+ * 7. m x n o'lchamli matritsa va k soni berilgan (0 <= k < n ).
+ * Matritsaning k- ustuni elementlarini chiqaruvchi programma tuzilsin.
  */
 public class Exercise7 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("m (rows): ");
+        int m = input.nextInt();
+
+        System.out.print("n (columns): ");
+        int n = input.nextInt();
+
+        System.out.print("k (0 <= k < n): ");
+        int k = input.nextInt();
+
+        if (k < 0) {
+            System.out.print("k must be equal or greater than zero");
+            return;
+        }
+
+        if (k >= n) {
+            System.out.print("k must be less than n");
+            return;
+        }
+
+        //1. insert elements to matrix
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        //2. get k-elements from this matrix
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int[] kRows = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                kRows[i] = matrix[i][j];
+//                System.out.print(matrix[0][j] + "\t");
+            }
+        }
+
+        System.out.println(Arrays.toString(kRows));
 
     }
 }
