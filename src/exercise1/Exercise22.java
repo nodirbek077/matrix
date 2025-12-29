7dc002f9ffc6ca69b8f200c1c66861274f3eeaaa
Nodirbek <n.nurkulov@insuranceon.uz>
2025-12-29 11:17:00 +0500

    Exercise23 finished

--- src/exercise1/Exercise22.java
+++ src/exercise1/Exercise22.java
@@ -1,4 +1,7 @@
 package exercise1;
+
+import util.ArrayUtil;
+
 /**
  * 22. m x n o'lchamli matritsa berilgan.
  * Faqat juft sonlardan iborat bo'lgan oxirgi satr nomerini aniqlovchi programma tuzilsin.
@@ -6,6 +9,30 @@ package exercise1;
  */
 public class Exercise22 {
     public static void main(String[] args) {
+        int[][] matrix = ArrayUtil.getArrayElements();
 
+        int lastEvenRowIndex = -1;
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            boolean allEven = true;
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] % 2 != 0) {
+                    allEven = false;
+                    break;
+                }
+            }
+
+            if (allEven) {
+                lastEvenRowIndex = i;
+                break;
+            }
+        }
+
+        if (lastEvenRowIndex != -1){
+            System.out.println("Last even row index: " + lastEvenRowIndex);
+        }else {
+            System.out.println("There is no even row in this array");
+        }
     }
 }
--- src/exercise1/Exercise23.java
+++ src/exercise1/Exercise23.java
@@ -1,10 +1,44 @@
 package exercise1;
+
+import util.ArrayUtil;
+
 /**
  * 23. m x n o'lchamli matritsa berilgan.
  * Bir xil elementlar soni eng ko'p bo'lgan birinchi ustunni aniqlovchi programma tuzilsin.
  */
 public class Exercise23 {
     public static void main(String[] args) {
+        int[][] matrix = ArrayUtil.getArrayElements();
 
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int firstBestColumn = 0;
+        int overallMaxRepeat = 0;
+        System.out.println();
+
+        for (int i = 0; i < cols; i++) {
+            int maxInThisCol = 0;
+
+            for (int j = 0; j < rows; j++) {
+                int count = 0;
+                for (int k = 0; k < rows; k++) {
+                    if (matrix[k][i] == matrix[j][i]) {
+                        count++;
+                    }
+                }
+
+                if (count > maxInThisCol) {
+                    maxInThisCol = count;
+                }
+            }
+
+            if (maxInThisCol > overallMaxRepeat) {
+                overallMaxRepeat = maxInThisCol;
+                firstBestColumn = i;
+            }
+        }
+
+        System.out.println("The first row index that the most same element: " + firstBestColumn);
+        System.out.println("Repeating count: " + overallMaxRepeat);
     }
 }
